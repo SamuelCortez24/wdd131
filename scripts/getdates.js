79517b680b0af1ca4782057a5213ed0c0261748b
SamuelCortez24 <pablo.samuel.cortez@gmail.com>
2025-05-10 14:39:26 -0300

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Current date in copyright
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Date of last modification of the document
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
